0353cf0fda66763ff2fd554360e826ba3bd5f057
pwolf15 <patrick.wolf@live.com>
2025-03-06 17:13:26 +0000

    Modify cube config

--- scripts/run-cube.sh
+++ scripts/run-cube.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python src/gmmcalib_viz.py --config_file_path ../config/cube_config.yaml --data ../point_clouds_overlap \
-    --sequence 121 122 123
+    --sequence 131 132 133 134 135 136 137 138 139 140 141 142 143 144 145 146 147 148 149 150
